3e8b81aee2c6f381308db451169ec4856bba1201
LattePanda for dev <dummy@muse-gr.com>
2023-06-04 18:01:14 +0900

    setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+source /opt/ros/melodic/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
+export LIBGL_ALWAYS_INDIRECT=1
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/muse/work/ORB_SLAM2/Examples/ROS
+
